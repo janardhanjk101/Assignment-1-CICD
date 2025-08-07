51ff1827455b6762a921cc8b63a13efea4969bb1
janardhanjk101 <janardhan.jsc@gmail.com>
2025-08-07 23:57:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 6000;
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${6000}`);
